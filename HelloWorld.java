0b91d2c159eb4abba47ad7d35fd9cf0c9947ce36
Hugo0322 <hugito2003@gmail.com>
2024-01-17 14:18:47 +0100

    Sum app

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Introduzca un número");
+        int first = s.nextInt();
+        System.out.println("Introduzca otro número para sumarlos");
+        int second = s.nextInt();
+        int totalSum = first + second;
+        System.out.println("La suma de ambos números es: " + totalSum);
 
     }
 }
